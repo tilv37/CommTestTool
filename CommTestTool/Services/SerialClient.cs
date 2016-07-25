5e8b923f106ad5dea632addd2e369c1965074667
dingshuo <tilv37@163.com>
2016-07-25 17:27:41 +0800

    加入报文接收间隔，和字节间接收间隔，提供串口充分的缓冲时间

--- CommTestTool/Services/SerialClient.cs
+++ CommTestTool/Services/SerialClient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO.Ports;
 using System.Linq;
 using System.Text;
@@ -45,12 +46,20 @@ namespace CommTestTool.Services
 
         private void OnReceived(object sender, SerialDataReceivedEventArgs e)
         {
+            //接收前等待，是缓冲区充分读取本包报文
+            System.Threading.Thread.Sleep(200);
             if (this._sp.BytesToRead <= 0)
                 return;
 
 
             byte[] bytes = new byte[_sp.BytesToRead];
-            _sp.Read(bytes, 0, _sp.BytesToRead);
+            for (int j = 0; j < bytes.Length; j++)
+            {
+                _sp.Read(bytes, j, 1);
+                //字节接收间间隔，依然是给串口读取一个缓冲时间
+                System.Threading.Thread.Sleep(10);
+            }
+            //_sp.Read(bytes, 0, _sp.BytesToRead);
             if (SerialDataReceivedEvent != null)
             {
                 SerialPortMessageReceivedEventArgs args =new SerialPortMessageReceivedEventArgs();
